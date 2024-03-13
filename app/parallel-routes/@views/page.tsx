15a167b8c7cfcea0364787170cbe5c4e216ef23e
Hugo Charré <hugo.charre@gmail.com>
2024-03-13 16:51:39 -0400

    edit

--- app/parallel-routes/@views/page.tsx
+++ app/parallel-routes/@views/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <Boundary labels={['@views/page.tsx']} size="small">
       <div className="prose prose-sm prose-invert max-w-none">
-        <h2 className="text-lg font-bold">Home</h2>
+        <h2 className="text-lg font-bold">Home - Edit</h2>
       </div>
     </Boundary>
   );
